c4cd31cf3dcae203aa93023ca0d3868d802c4001
ClaudiuBu <37150852+ClaudiuBu@users.noreply.github.com>
2019-10-17 21:19:06 +0300

    Added Dificulty menu. Js not implement. Only cosmetic purposes for now

--- js/main-menu.js
+++ js/main-menu.js
@@ -5,9 +5,10 @@ alert("main-menu-script added");
 function singlePlayerClick(){
     console.log("Ajung in scriptul de single-player");
     $('.menu-container').fadeOut( "slow", function() {
-        $('.body-Tic-Tac-Toe').removeClass("invisible").fadeIn("slow",function(){
-            //animation comple;
-        }).addClass("visible");
+        $(".singple-p-container").removeClass("invisible");
+        // $('.body-Tic-Tac-Toe').removeClass("invisible").fadeIn( "fast", function() {
+        //     // Animation complete
+        //   });
       });
 
 }
